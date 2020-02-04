b17a6bc444def2f7a2fe0153406ca2733c315bce
Stanislav <lemeshaevs@gmail.com>
2020-02-04 22:37:07 +0300

    Написал класс Яблоко

--- SnakeLS/SnakeLS/Apple.swift
+++ SnakeLS/SnakeLS/Apple.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Станислав Лемешаев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+// яблоко для змейки
+class Apple: SKShapeNode {
+    // определяем, как оно будет отрисовываться
+    convenience init(position: CGPoint) {
+        self.init()
+        // рисуем круг
+        path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 10, height: 10)).cgPath
+        // заливаем красным
+        fillColor = UIColor.red
+        // рамка тоже красная
+        strokeColor = UIColor.red
+        // ширина рамки 5 поинтов
+        lineWidth = 5
+        self.position = position
+    }
+}
