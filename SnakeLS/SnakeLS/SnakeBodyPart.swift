5226673b4f2e2e68d3c6cc206a4dbe1f670cafe7
Stanislav <lemeshaevs@gmail.com>
2020-02-05 20:34:31 +0300

    Написал класс SnakeBodyPart

--- SnakeLS/SnakeLS/SnakeBodyPart.swift
+++ SnakeLS/SnakeLS/SnakeBodyPart.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2020 Станислав Лемешаев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+class SnakeBodyPart: SKShapeNode {
+    
+    let diameter = 10.0
+    
+    // добавляем конструктор
+    init(atPoint point: CGPoint) {
+        super.init()
+        // рисуем круглый элемент
+        path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: CGFloat(diameter), height: CGFloat(diameter))).cgPath
+        // цвет рамки и заливки
+        fillColor = UIColor.green
+        strokeColor = UIColor.green
+        // ширина рамки 5 поинтов
+        lineWidth = 5
+        // размещаем элемент в переданной точке
+        self.position = point
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
