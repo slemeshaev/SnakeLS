86500aaac5a2098b3106aee6f0cce32f76257621
Stanislav <lemeshaevs@gmail.com>
2020-02-05 20:34:08 +0300

    Создал файл SnakeBodyPart

---
+++ SnakeLS/SnakeLS/SnakeBodyPart.swift
@@ -0,0 +1,9 @@
+//
+//  SnakeBodyPart.swift
+//  SnakeLS
+//
+//  Created by Станислав Лемешаев on 05.02.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
