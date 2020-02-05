3e98fc1f2ce9cbcb12b12686f3534205e26c20d6
Stanislav <lemeshaevs@gmail.com>
2020-02-05 20:39:37 +0300

    Написал класс SnakeHead

--- SnakeLS/SnakeLS/SnakeHead.swift
+++ SnakeLS/SnakeLS/SnakeHead.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Станислав Лемешаев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SnakeHead: SnakeBodyPart {
+    
+    override init(atPoint point: CGPoint) {
+        super.init(atPoint: point)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
