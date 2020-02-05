014901989ef5c7d1f91ab3a450a38e398b7f7a7f
Stanislav <lemeshaevs@gmail.com>
2020-02-05 21:10:36 +0300

    Написал класс Snake

--- SnakeLS/SnakeLS/Snake.swift
+++ SnakeLS/SnakeLS/Snake.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2020 Станислав Лемешаев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+// змейка
+class Snake: SKShapeNode {
+    
+    // массив, где хранятся сегменты тела
+    var body = [SnakeBodyPart]()
+    
+    // конструктор
+    convenience init(atPoint point: CGPoint) {
+        self.init()
+        // змейка начинается с головы, создадим ее
+        let head = SnakeHead(atPoint: point)
+        // и добавим ее в массив
+        body.append(head)
+        // и сделаем ее дочерним объектом
+        addChild(head)
+    }
+    
+    // метод добавляет еще один сегмент тела
+    func addBodyPart() {
+        // инстанцируем сегмент
+        let newBodyPart = SnakeBodyPart(atPoint: CGPoint(x: body[0].position.x, y: body[0].position.y))
+        // добавляем его в массив
+        body.append(newBodyPart)
+        // делаем дочерним объектом
+        addChild(newBodyPart)
+    }
+}
