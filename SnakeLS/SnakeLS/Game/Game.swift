f2ffa8b4bcaae80a2f44969fedf6e0c8584f4862
Stanislav <lemeshaevs@gmail.com>
2020-02-27 23:27:05 +0300

    Создал файлы для записей результата

---
+++ SnakeLS/SnakeLS/Game/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  SnakeLS
+//
+//  Created by Станислав Лемешаев on 27.02.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
---
+++ SnakeLS/SnakeLS/Game/Record.swift
@@ -0,0 +1,9 @@
+//
+//  Record.swift
+//  SnakeLS
+//
+//  Created by Станислав Лемешаев on 27.02.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
