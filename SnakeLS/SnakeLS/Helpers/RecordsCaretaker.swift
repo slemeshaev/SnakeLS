011bd14f293a0a4edf840e9fbe661f8f5f363316
Stanislav <lemeshaevs@gmail.com>
2020-04-07 19:44:43 +0300

    Создал класс RecordsCaretaker

---
+++ SnakeLS/SnakeLS/Helpers/RecordsCaretaker.swift
@@ -0,0 +1,9 @@
+//
+//  RecordsCaretaker.swift
+//  SnakeLS
+//
+//  Created by Станислав Лемешаев on 07.04.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
