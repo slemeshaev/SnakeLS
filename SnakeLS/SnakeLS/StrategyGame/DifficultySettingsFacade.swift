2425e31b597ed4bc01840a43b4c2bf909fdfdb96
Stanislav <lemeshaevs@gmail.com>
2020-06-04 19:58:43 +0300

    Добавил файл для сложности игры Facade

---
+++ SnakeLS/SnakeLS/StrategyGame/DifficultySettingsFacade.swift
@@ -0,0 +1,9 @@
+//
+//  DifficultySettingsFacade.swift
+//  SnakeLS
+//
+//  Created by Станислав Лемешаев on 04.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
